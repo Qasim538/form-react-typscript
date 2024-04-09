ec00fa746b86ebe9a996bea46785931aa66fa5a9
Qasim <mq12113@gmail.com>
2024-04-09 22:08:26 +0100

    updated form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { useMultistepForm } from "./useMultistepForm"
 
 function App()  {
+  const { steps, currentStepIndex, step } = useMultistepForm([
+  <div>One</div>,
+  <div>Two</div>
+])
   return (
-    <div>
-      <h1>Hi</h1>
+    <div style={{
+      position: "relative",
+      background: "white",
+      border: "1px solid black",
+      padding: "2rem",
+      margin: "1rem",
+      borderRadius: ".5rem",
+      fontFamily: "Arial"
+
+    }}>
+      <form>
+        <div 
+        style={{
+          position: "absolute",
+          top: ".5rem",
+          right: ".5rem"
+        }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div></div>
+
+      </form>
     </div>
   )
 }
